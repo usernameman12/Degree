249aa7897de9b919aa21114cc70645e6af7b42c7
Oreo <the55thone@gmail.com>
2025-04-04 15:57:55 -0700

    add uv static

---
+++ static/uv/uv.config.js
@@ -0,0 +1,10 @@
+self.__uv$config = {
+    prefix: '/static/go/',
+    bare: 'https://sadasdasd.100mountain.com/bare/',
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    handler: '/active/uv/uv.handler.js',
+    bundle: '/active/uv/uv.bundle.js',
+    config: '/active/uv/uv.config.js',
+    sw: '/active/uv/uv.sw.js',
+};
