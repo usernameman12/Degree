4caf0e25024523f659348dcded5afe5b99857dc0
UVUE <wanted.glitch@gmail.com>
2025-04-04 23:51:34 -0400

    I want to yell angrily but I’m speechless

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,10 +1,10 @@
 self.__uv$config = {
-    prefix: '/static/go/',
-    bare: 'https://sadasdasd.100mountain.com/bare/',
+    prefix: '/static/uv/go/', //it can't do da going unless its pointing to /uv/go
+    bare: 'https://sadasdasd.100mountain.com/bare/', // not even gonna question it
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: '/active/uv/uv.handler.js',
-    bundle: '/active/uv/uv.bundle.js',
-    config: '/active/uv/uv.config.js',
-    sw: '/active/uv/uv.sw.js',
+    handler: '/static/uv.handler.js', // /active doesn't exist now does it
+    bundle: '/static/uv.bundle.js',
+    config: '/static/uv.config.js',
+    sw: '/static/uv.sw.js',
 };
